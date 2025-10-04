94325adab83c447baddbd4476288ddaa9a5edc06
yameili781-coder <yameili781@gmail.com>
2025-10-04 08:04:32 -0700

    Update entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+# 设置 DNS
 printf "nameserver 127.0.0.11\nnameserver 8.8.4.4\nnameserver 223.5.5.5\n" > /etc/resolv.conf
+
+# 修复 app 权限
+chmod 755 /dashboard/app
+
+# 运行应用
 exec /dashboard/app
